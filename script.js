b42e9db87b8b95dcd424bccde4349493b08c0655
Matt Boston <mattbos10@icloud.com>
2021-10-12 09:23:47 -0500

    added bonus

--- script.js
+++ script.js
@@ -25,4 +25,38 @@ while (harryPotterMovies < 8) {
 }
 // 4c
 console.log(harryPotterMovies);
+
 // BONUS
+// 5a
+const hogwartsHouses = [
+    "Gryffindor",
+    "Hufflepuff",
+    "Ravenclaw",
+    "Slytherin"
+];
+// 5b
+for (houses of hogwartsHouses) {
+    for (letters of houses){
+        console.log(letters);
+    }
+};
+// 6a
+const quote = [
+    "Yer",
+    "A",
+    "Wizard",
+    "Harry"
+];
+// 6b
+let sentence = "";
+for (word of quote) {
+    sentence += word + " ";           //same as sentence = sentence + `${word} `;
+}
+console.log(sentence);
+
+// OR
+
+// while (quote !== sentence) {
+//     console.log(quote.join(" "));
+//     break;
+// }
